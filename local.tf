78ae5bc6ee3fd564008d3f973337b90ed18ed9a2
munnep <patrick.munne@hashicorp.com>
2023-04-25 13:45:30 +0200

    small changes

--- local.tf
+++ local.tf
@@ -3,4 +3,7 @@ locals {
   az2                    = "${var.region}b"
   rds_snapshots          = toset(var.rds_snapshots)
   s3_bucket_restore_time = var.rds_snapshot_to_restore == null ? "There is no restore done" : module.snapshot_restore[0].restore_s3_bucket_to_rds_snapshot
+  tags = {
+    "OwnedBy" = "patrick.munne@hashicorp.com"
+  }
 }
